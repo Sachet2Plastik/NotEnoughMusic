6f48c5dbca52afbbaa34ff99832d5664df228981
Sachet2Plastik <sacha.dacalor-fourcade@epitech.eu>
2026-07-20 00:38:09 +0400

    [+] Init Repo: Adding Fabric Mod Template

---
+++ src/client/java/sachet2plastik/moremusicdiscs/client/MoreMusicDiscsClient.java
@@ -0,0 +1,10 @@
+package sachet2plastik.moremusicdiscs.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreMusicDiscsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
